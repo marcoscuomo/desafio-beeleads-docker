e1cf2faba009c14f544457fed821a10b965466b7
Marcos Souza <bytecore@gmail.com>
2021-12-05 23:48:43 -0300

    Will create the function of list all customers

--- src/shared/infra/http/routes/customer.routes.ts
+++ src/shared/infra/http/routes/customer.routes.ts
@@ -3,13 +3,16 @@ import { Router } from 'express';
 import { ensureAuthenticate } from '@shared/infra/middlewares/ensureAuthenticate';
 import { CreateCustomerController } from '@modules/customers/useCases/createCustomer/CreateCustomerController';
 import { UpdateCustomerController } from '@modules/customers/useCases/updateCustomer/UpdateCustomerController';
+import { ListCustomerController } from '@modules/customers/useCases/listCustomers/ListCustomersController';
 
 const customerRouter = Router();
 
 const createCustomerController = new CreateCustomerController();
 const updateCustomerController = new UpdateCustomerController();
+const listCustomerController = new ListCustomerController();
 
 customerRouter.post('/', ensureAuthenticate, createCustomerController.handle);
 customerRouter.put('/', ensureAuthenticate, updateCustomerController.handle);
+customerRouter.get('/', ensureAuthenticate, listCustomerController.handle);
 
 export { customerRouter };
